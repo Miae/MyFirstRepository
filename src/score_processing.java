af6c3a860e73deca7761aeb3d3180ae3a8c9993b
lma0825 <lma95082528@gmail.com>
2013-06-02 16:00:06 +0900

    scanner사용하여 입력바꾸고 배열에 과목이름과 점수 저장하기

--- src/score_processing.java
+++ src/score_processing.java
@@ -8,34 +8,35 @@ public class score_processing {
 		System.out.println("안녕하세요 미림여자정보과학고등학교 3학년 인터랙티브미디어과 이미애입니다.");
 		System.out.println("★성적처리프로그램★");
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("학교를 입력해주세요 : ");
-		String schoolname = br.readLine();
+		String schoolname = input.next();
 		System.out.print("학년을 입력해주세요.(숫자만 입력) : ");
-		int grade = Integer.parseInt(br.readLine());
+		int grade = input.nextInt();
 		System.out.print("이름을 입력해주세요. : ");
-		String name = br.readLine();
+		String name = input.next();
 		
 		System.out.println("안녕하세요. " + schoolname +" " + grade + "학년 " + name + "학생 반갑습니다.");
 		
 		System.out.print("점수를 입력하실 과목수를 입력해주세요. : ");
-		int subjectnum = Integer.parseInt(br.readLine());
+		int subjectnum =input.nextInt();
 		
-		String subjectsave[] = null;
 		String subject;
-		int subjectgrade;
-		int subgradesave[] = null;
-		int total = 0;
-		
-		for(int i = 0; i<=subjectnum; i++){
+		String subjectsave[] = new String[subjectnum];
+		double subjectgrade;
+		double subgradesave[] = new double[subjectnum];
+
+		for(int i = 1; i<=subjectnum; i++){
 			System.out.print("과목을 입력해주세요 : ");
-			subject = br.readLine();
+			subject = input.next();
+			subjectsave[i-0] = subject;
+			System.out.println(subjectsave[i-0]);
 			System.out.print(subject + "의 점수를 입력해주세요. : ");
-			subjectgrade = Integer.parseInt(br.readLine());
-			total = subjectgrade/subjectnum;
-			System.out.println(total);
+			subjectgrade = input.nextDouble();
+			subgradesave[i-0] = subjectgrade;
+			System.out.println(subgradesave[i-0]);
+		
 		}
 	
 	}

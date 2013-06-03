156545228845d6672af9e2dfcbf474090b287415
lma0825 <lma95082528@gmail.com>
2013-06-03 10:45:53 +0900

    GUI로 만들기 + 윈도우리스너 추가

--- src/score_processing.java
+++ src/score_processing.java
@@ -1,27 +1,61 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 
-public class score_processing {
+public class score_processing extends Frame implements WindowListener {
+	GridLayout G1 = new GridLayout(3,1);
+
+	Panel P1 = new Panel();
+
+	Label L1 = new Label("안녕하세요.미림여자정보과학고등학교 3학년 이미애입니다.");
+	Label L2 = new Label("★성적처리프로그램★");
+
+	score_processing(){
+		super("성적처리 프로그램");
+		//P1.setLayout(G1);
+		P1.add(L1);
+		P1.add(L2);
+
+		this.add(P1);
+		this.addWindowListener(this);
+		this.setSize(400,400);
+		this.setVisible(true);
+	}
+	public void windowDeactivated(WindowEvent e){};
+	public void windowActivated(WindowEvent e){};
+	public void windowClosed(WindowEvent e){};
+	public void windowClosing(WindowEvent e){
+		System.exit(0);
+	};
+	public void windowDeiconified(WindowEvent e){};
+	public void windowIconified(WindowEvent e){};
+	public void windowOpened(WindowEvent e){};
 	public static void main(String args[]) throws Exception {
+
 		System.out.println("안녕하세요 미림여자정보과학고등학교 3학년 인터랙티브미디어과 이미애입니다.");
 		System.out.println("★성적처리프로그램★");
-		while(true){
+
+		score_processing s_p = new score_processing();
+		/*while(true){
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("학교를 입력해주세요 : ");
 		String schoolname = input.next();
 		System.out.print("학년을 입력해주세요.(숫자만 입력) : ");
 		int grade = input.nextInt();
 		System.out.print("이름을 입력해주세요. : ");
 		String name = input.next();
-		
+
 		System.out.println("안녕하세요. " + schoolname +" " + grade + "학년 " + name + "학생 반갑습니다.");
-		
+
 		System.out.print("점수를 입력하실 과목수를 입력해주세요. : ");
 		int subjectnum =input.nextInt();
-		
+
 		String subject;
 		String subjectsave[] = new String[subjectnum];
 		double subjectgrade;
@@ -34,7 +68,7 @@ public class score_processing {
 			subject = input.next();
 			subjectsave[i-1] = subject;
 			//System.out.println(subjectsave[i-1]);
-			
+
 			System.out.print(subject + "의 점수를 입력해주세요. : ");
 			subjectgrade = input.nextDouble();
 			if(subjectgrade>100){
@@ -52,6 +86,6 @@ public class score_processing {
 		avg = total/subjectnum;
 		String avg2 = String.format("%.2f", avg);
 		System.out.println(name + "학생의 평균은 " + avg2 +"입니다.");
-	}
+	}*/
 	}
 }
